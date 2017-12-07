ff02ef6c7b20314fb148891d20f4fcdc8036a8fb
Edgar Zanella Alvarenga <e@vaz.io>
2017-12-07 11:22:21 +0100

    Adding horizontal bar graph component

---
+++ src/MyHBar.vue
@@ -0,0 +1,28 @@
+<script>
+
+import { HorizontalBar, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default {
+  extends: HorizontalBar,
+  mixins: [reactiveProp],
+  name: 'MyHBar',
+  props: ['options'],
+  data () {
+    return {
+      datacollection: null
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.options)
+  },
+  watch: {
+    chartData: function() {
+      //this.renderChart(this.data, this.options);
+      this.$data._chart.destroy()
+      this.renderChart(this.chartData, this.options)
+    }
+  }
+}
+
+</script>
